abb0975bed06efb1e7be529adc4faa6286f8d171
bajoao <bartolomeugasparbg@gmail.com>
2025-09-19 01:46:20 +0100

    terminado

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bajoao <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 00:11:37 by bajoao            #+#    #+#             */
-/*   Updated: 2025/09/19 01:33:18 by bajoao           ###   ########.fr       */
+/*   Updated: 2025/09/19 01:45:47 by bajoao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,44 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stddef.h>
-void	ft_tolower(char *s)
-{
-	for (int i = 0; s[i]; i++)
-		s[i] = tolower(s[i]);
-}
-void	rem_ispunct(char * str)
-{
-	for (int i = 0; i < strlen(str); i++)
-	{
-		if (ispunct(str[i]))
-			str[i] = ' ';
-	}
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void ft_tolower(char *s) {
+    for (int i = 0; s[i]; i++)
+        s[i] = tolower(s[i]);
 }
 
-int main(int argc, char **argv)
-{
-	if (argc < 1)
-		printf ("numero de arqumento invalido");
-	//char	*word;
-//	int	i = 0;
-///	printf ("Digite a palavra");
-//	scanf ("%s", argv[1]);
-	while (argv[1])
-	{
-		ft_tolower(argv[1]);
-		rem_ispunct(argv[1]);
-		strtok(argv[1], " \t\n");
-	}
-	printf ("%s", argv[1]);
+void rem_ispunct(char *str) {
+    for (int i = 0; str[i]; i++) {
+        if (ispunct(str[i]))
+            str[i] = ' ';
+    }
 }
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Número de argumentos inválido\n");
+        return 1;
+    }
+
+   
+    char buffer[1000];
+    strncpy(buffer, argv[1], sizeof(buffer));
+    buffer[sizeof(buffer) - 1] = '\0';
+
+    ft_tolower(buffer);
+    rem_ispunct(buffer);
+
+  
+    char *token = strtok(buffer, " \t\n");
+    while (token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, " \t\n");
+    }
+
+    return 0;
+}
+
